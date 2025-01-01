e8cf3978cdbb9002898d24d7b85d5991ca316e17
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-01 18:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 // Complete the js code
 function Car(make, model) {
 	this.make = make;
-  this.model = model;
-
+    this.model = model;
 }
 
 Car.prototype.getMakeModel = function() {
-  return this.make + " " + this.model;
+    return this.make + " " + this.model;
 }
 
 function SportsCar(make, model, topSpeed) {
-	  Car.call(this, make, model);
-  this.topSpeed = topSpeed;
-
-	SportsCar.prototype = Object.create(Car.prototype);
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
 
+SportsCar.prototype = Object.create(Car.prototype);
+
 SportsCar.prototype.getTopSpeed = function() {
-  return this.topSpeed;
+    return this.topSpeed;
 }
 
 // Do not change the code below
